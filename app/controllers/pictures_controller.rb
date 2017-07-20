2a35d99b738988a0e56269ff51ea295e7c75f7ee
Belizario Rocha <rocha.belizario@chicagobooth.edu>
2017-07-20 00:10:20 +0000

    V1.0

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,22 @@
+class PicturesController < ApplicationController
+    def new_form
+        render("pic_templates/new_form.html.erb")
+    end
+    def create_row
+        render("pic_templates/new_form.html.erb")
+    end
+    def index
+        render("pic_templates/new_form.html.erb")
+    end
+    def show
+        render("pic_templates/new_form.html.erb")
+    end
+    def edit_form
+        render("pic_templates/new_form.html.erb")
+    end
+    def update_row
+        render("pic_templates/new_form.html.erb")
+    end
+
+
+end
